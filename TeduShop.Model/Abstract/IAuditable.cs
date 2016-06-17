a09565ff2790cbd298c45a03bcddb86c064a5358
Phat Vo Tan <phatvt93@gmail.com>
2016-06-17 12:58:12 +0700

    Update Model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+        bool Status { set; get; }
+
+
+    }
+}
