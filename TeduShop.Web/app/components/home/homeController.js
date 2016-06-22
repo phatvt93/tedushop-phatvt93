3ba9e7260ef51269ccf1d9230057d590fc342335
Phat Vo Tan <phatvt93@gmail.com>
2016-06-23 06:22:59 +0700

    route impliment for app

---
+++ TeduShop.Web/app/components/home/homeController.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+
+(function(app) {
+    app.controller('homeController', homeController);
+
+    function homeController() {
+        
+    }
+})(angular.module('tedushop'));
