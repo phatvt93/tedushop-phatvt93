6be4cceeec463fc627688d569137ef17ac888e5c
Phat Vo Tan <phatvt93@gmail.com>
2016-06-30 00:41:41 +0700

    add template for client component

---
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace TeduShop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
+    }
+}
